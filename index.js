031f00470447d7071b39aa8a981a4f684fea2e05
Danielle Radebe <radebedanielle@gmail.com>
2023-05-15 13:36:41 +0200

    sprites no longer move

--- index.js
+++ index.js
@@ -110,19 +110,16 @@ class Sprite {
     }
 
     update(){
-        //whenever we call update we call draw, this is for our animations
-        //this means that our position.y is going to have 10 frames added to it everytime we loop
+        //whenever we call update we call draw to draw our player
         this.draw("red")
+        //making our sprite fall along the y axis until it hits the ground by constantly adding
+        //velocity to our y axis
         this.position.y += this.velocity.y
-        //now we change the velocity from 0 to something else
-        //we change the position of y constantly and make it add the value of our velocity
-        //over and over again making it go down
 
-        //so what we have done here is when we hit the 'arror right' key our position
-        //moves forward by our x axis velocity value
-        this.position.x += this.velocity.x
 
         //this stops our players from falling when they hit the floor
+        //when our position y plus our height plus our velocity is more equal to
+        //our canvas height then we stop our velocity sprite is zero and stops moving
         if(this.position.y + this.height + this.velocity.y >= canvas.height){
             this.velocity.y = 0
             //this just ensures that gravity is real so our player doesnt just chill
@@ -135,7 +132,7 @@ class Sprite {
 //so this is our first player and its called player
 const player = new Sprite({
     position:{
-        x:0, 
+        x:40, 
         y:0
     },
     velocity: {
@@ -150,7 +147,7 @@ const player = new Sprite({
 //we ensure the position is at the opposite side of our screen by changing the x and y values
 const enemy = new Sprite({
     position:{
-        x:400, 
+        x:900, 
         y:100
     },
     velocity: {
@@ -181,15 +178,6 @@ console.log(player)
 //keys that our game uses
 //all set to false until they are actually pressed
 const keys = {
-    ArrowRight: {
-        pressed: false
-    },
-    ArrowLeft: {
-        pressed: false
-    },
-    ArrowUp: {
-        pressed: false
-    },
     d: {
         pressed: false
     },
@@ -200,7 +188,6 @@ const keys = {
         pressed: false
     }
 }
-let lastKey
 
 //now we are going to create an animation loop 
 //our players will fall to the bottom of the screen and be constantly moving
@@ -220,17 +207,6 @@ function animate(){
     //by constantly calling draw
     player.update()
     enemy.update()
-    // rock.update()
-
-    player.velocity.x = 0
-    //so we monitor to see if our keys are being pressed
-    //so instead of changing velocity to 0 if any button is lifted despite the other being pressed down
-    // we have a true and false and so long as left or right is true our player will keep moving
-    if (keys.ArrowRight.pressed && lastKey === 'ArrowRight'){
-        player.velocity.x = 3
-    } else if (keys.ArrowLeft.pressed && lastKey === 'ArrowLeft'){
-        player.velocity.x = -3
-    }
 
     if (keys.d.pressed){
         console.log(rock)
@@ -257,24 +233,6 @@ animate()
 // and now we have certain things we do in event of a certain key pressed
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
-        case 'ArrowRight':
-            //so when we press our arrow right key then our value called pressed that is assigned
-            //to our arrow right in keys is changed to true
-            keys.ArrowRight.pressed = true
-            lastKey = 'ArrowRight'
-            break
-        case 'ArrowLeft':
-            //same here so when we press down our left key then pressed changes to true despite if right 
-            //is being pressed down or not
-            keys.ArrowLeft.pressed = true
-            lastKey = 'ArrowLeft'
-            break
-        case 'ArrowUp':
-            //here we add a jump feature so when we press up our player jumps up or bounces
-            //so if we press up we set arrow up to true and our player jumps
-            keys.ArrowUp.pressed = true
-            player.velocity.y = -75
-            break
         case 'd':
             keys.d.pressed = true
             rock = new Rock({
@@ -310,29 +268,6 @@ window.addEventListener('keydown', (event) => {
                 x:12,
                 }
             })
-            break
-    }
-    console.log(event.key);
-})
-
-//second event listener that looks for key up
-//so when we let go of our d key a different action is carried out
-window.addEventListener('keyup', (event) => {
-    switch (event.key) {
-        case 'ArrowRight':
-            //when the right key is no longer pressed then we change arrowright.pressed back to false
-            //so our velocity changes back to 0 and our player stops moving
-            keys.ArrowRight.pressed = false
-            break
-        case 'ArrowLeft':
-            //same here
-            keys.ArrowLeft.pressed = false
-            break
-        case 'ArrowUp':
-            //here we add a jump feature so when we press up our player jumps up or bounces
-            //so if we press up we set arrow up to true and our player jumps
-            keys.ArrowUp.pressed = false
-            player.velocity.y = 0
             break
     }
     console.log(event.key);
